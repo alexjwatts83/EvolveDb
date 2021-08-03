f693acbee2d623558d9493e04a48cede701de130
Alex Watts <alexjwatts83@gmail.com>
2021-08-04 06:54:41 +1000

    Added evole nuget

--- src/EvolveDb.Api/Program.cs
+++ src/EvolveDb.Api/Program.cs
@@ -13,7 +13,11 @@ namespace EvolveDb.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            RunDbMigrations.Run(host);
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,23 @@ namespace EvolveDb.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    internal static class RunDbMigrations
+    {
+        internal static void Run(IHost host)
+        {
+            // Run db Migrations
+            //var options = GetFluentMigratorOptions(host);
+
+            //var serviceProvider = PersistenceDbMigrations.CreateServices(options.DbConnectionString, options.Tags);
+
+            //PersistenceDbMigrations.EnsureDatabase(options.MasterDb, options.MainDbName);
+
+            //// Put the database update into a scope to ensure that all resources will be disposed.
+            //using var scope = serviceProvider.CreateScope();
+
+            //PersistenceDbMigrations.UpdateDatabase(scope.ServiceProvider);
+        }
     }
 }
